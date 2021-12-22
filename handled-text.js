65cbaeca44a1fa9362f6b05e72fa8cb483137d02
yaroslav.pylaev <56zeBVPax8kPgjM>
2021-12-22 22:26:41 +0300

    TGB-3: add spotify auth handler

--- handled-text.js
+++ handled-text.js
@@ -4,7 +4,9 @@ module.exports = {
         INFO: '/info',
         AUTH_OPTIONS: '/auth_options',
         SPOTIFY_AUTH: '/spotify_auth',
-        YANDEX_MUSIC_AUTH: '/yandex_music_auth'
+        YANDEX_MUSIC_AUTH: '/yandex_music_auth',
+        SYNC_OPTIONS: '/sync_options',
+        SPOTIFY_SYNC_TO_YANDEX: '/spotify_sync_to_yandex'
     },
     BUTTONS : {
         AUTH: 'Авторизация',
