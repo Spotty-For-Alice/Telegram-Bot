f3e30a072c3c0fb62e1bcc230dc7ec6c3dad7369
yaroslav.pylaev <56zeBVPax8kPgjM>
2022-04-03 13:47:58 +0300

    add webhook

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const packageInfo = require('../package.json');
 const TELEGRAM_API = require('node-telegram-bot-api');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+
+var server = app.listen(process.env.PORT, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Web server started at http://%s:%s', host, port);
+});
 
 const Bot = require('./bot');
 
@@ -9,4 +23,3 @@ function setUpBot() {
 }
 
 setUpBot();
-
